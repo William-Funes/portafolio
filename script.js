086d9dded807483b9c1632be35b06da2d7447389
William-Funes <williamandrade1605@gmail.com>
2025-06-10 12:41:05 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrar(id) {
+  document.getElementById('c1').style.display = 'none';
+  document.getElementById('c2').style.display = 'none';
+  document.getElementById('c3').style.display = 'none';
+  document.getElementById(id).style.display = 'block';
+}
